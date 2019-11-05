97b99ffdecd8b7f6bd6b1c9ad7219474ce80de81
Elton Rafael <elton86@gmail.com>
2019-11-05 16:08:45 -0300

    Sem mudanças.
    Apenas identação.

--- PCA - PLS/Teste ROPLS.R
+++ PCA - PLS/Teste ROPLS.R
@@ -9,35 +9,32 @@ strF(dataMatrix)
 imageF(dataMatrix)#imagem da matriz de ddos
 
 #o comando opls gera o PCA neste pacote
-#obs: com este comando tambem é gerado um sumário do modelo
-sacurine.pca <- opls(dataMatrix)
-
-
+#obs: com este comando tambem ? gerado um sum?rio do modelo
+sacurine.pca <- opls(sacurine$dataMatrix)
 
 genderFc <- sampleMetadata[, "gender"] #Cria um vetor com masculino e feminino
 plot(sacurine.pca,
      typeVc = "x-score",
-     parAsColFcVn = genderFc) #cria um grafico pelo factor genedro
+     parAsColFcVn = genderFc) #cria um grafico pelo factor gÃªnero
 
 
-#testanto exemplo com condição de peso
-valor_bmi <- sampleMetadata[,"bmi"]
+#testanto exemplo com condi??o de peso
+valor_bmi <- sacurine$sampleMetadata[,"bmi"]
 BMI_teste <- data.frame(valor_bmi)
 BMI_teste$Condicao <- ifelse(valor_bmi >= 30, "Obeso", "normal")
 BMI_teste$Condicao <- as.factor(BMI_teste$Condicao)
 plot(sacurine.pca,
-     typeVc = "x-score", #gráficos complementares ex: ("outlier", "predict-train", "xy-score", "xy-weight")
+     typeVc = "x-score", #gr?ficos complementares ex: ("outlier", "predict-train", "xy-score", "xy-weight")
      parAsColFcVn = BMI_teste$Condicao) 
 
 names(sampleMetadata)
 
-#personalização dos gráficos
+#personaliza??o dos gr?ficos
 plot(sacurine.pca,
      typeVc = "x-score",
      parAsColFcVn = genderFc,
      parLabVc = as.character(sampleMetadata[, "age"]),
      parPaletteVc = c("blue2", "magenta"))
-
 
 
 #TRABALHANDO COM PLS E PLS-DA
@@ -47,7 +44,7 @@ sacurine.plsda <- opls(dataMatrix, genderFc)
 sacurine.oplsda <- opls(dataMatrix, genderFc,
                         predI = 1, orthoI = NA
                                )
-#customização de gráficos 
+#customiza??o de gr?ficos 
 plot(sacurine.oplsda ,
      typeVc = "x-variance",
      parAsColFcVn = genderFc,
@@ -55,13 +52,12 @@ plot(sacurine.oplsda ,
      parPaletteVc = c("red", "green3"))
 
 
-
 #teste PCA - Biplot
 teste_pca <- pca(sacurine$sampleMetadata[,1:3])
 biplot(teste_pca)
 
 
-#tipos de gráficos ***********************************************
+#tipos de gr?ficos ***********************************************
 
 #'typeVc' elements must be either 'correlation', 'outlier', 'overview', 'permutation', 
 #''predict-train', 'predict-test', 'x-loading', 'x-score', 'x-variance', 'xy-score', 'xy-weight'
